1569082e23dc21418d983c62ae527cfb7e1e5dd5
NOP-Gate <Krailon@users.noreply.github.com>
2022-06-10 23:43:59 -0700

    Update rainbow.js

--- cheat/rainbow.js
+++ cheat/rainbow.js
@@ -1,7 +1,7 @@
 /** @param {NS} ns */
 export async function main(ns) {
 	/*
-	Unlocks Rainbow exploit.
+	Unlocks Rainbow exploit for SF -1.
 	Sorry but I'm not revealing the plaintext since
 	it's a fun challenge that's surprisingly easy
 	if you do a bit of research.
