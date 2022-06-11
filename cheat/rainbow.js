ae1e5af626034cf22c0bafb582a2ccd93b39393e
NOP-Gate <Krailon@users.noreply.github.com>
2022-06-10 23:39:27 -0700

    Create rainbow.js

---
+++ cheat/rainbow.js
@@ -0,0 +1,8 @@
+/** @param {NS} ns */
+export async function main(ns) {
+  /*
+    Sorry but I'm not revealing the full solution since
+    it's a fun challenge with a surprisingly easy solution.
+  */
+	ns.rainbow('REDACTED');
+}
