4b8407581930818c7620e41231bc05d1e7f536db
NOP-Gate <Krailon@users.noreply.github.com>
2022-06-10 23:44:15 -0700

    Update bypass.js

--- cheat/bypass.js
+++ cheat/bypass.js
@@ -1,6 +1,6 @@
 /** @param {NS} ns */
 export async function main(ns) {
-	// Unlocks Bypass exploit
+	// Unlocks Bypass exploit for SF -1
     ns.tprint('Running bypass...');
 	ns.bypass(top['document']);
 }
