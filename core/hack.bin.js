0abb3cf61d279b6570e67c79a0c8fe1cc8ebab07
NOP-Gate <Krailon@users.noreply.github.com>
2023-10-07 22:04:19 -0700

    Create hack.bin.js

---
+++ core/hack.bin.js
@@ -0,0 +1,17 @@
+const runtimeMultiplier = 1.0;
+
+/** @param {NS} ns */
+export async function main(ns) {
+	const target = ns.args[0] || 'foodnstuff';
+	const next = ns.args[2];
+	//const runtime = runtimeMultiplier * ns.getHackTime(target);
+
+	do {
+		if(next) {
+			let currentTime = performance.now();
+			await ns.asleep(next - currentTime);
+		}
+
+		await ns.hack(target);
+	} while (ns.args[1] && ns.getServerMoneyAvailable(target) > 0);
+}
